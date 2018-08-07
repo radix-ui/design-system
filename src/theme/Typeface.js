f03a5ba1ca0df128c5fdf80a3a7fb0f8b1c6fc46
Colm Tuite <colmtuite@gmail.com>
2018-08-07 11:40:22 +0100

    Add typeface property to theme

---
+++ src/theme/Typeface.js
@@ -0,0 +1,2 @@
+export const UNTITLEDSANS = '"Untitled Sans",apple-system,BlinkMacSystemFont,"Helvetica Neue",helvetica,arial,sans-serif';
+export const MONO = 'Consolas,Liberation Mono,Menlo,Courier,monospace';
