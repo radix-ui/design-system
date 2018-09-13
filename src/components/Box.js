536720a56f99b0fcaab78c5e824ff9228260540f
Colm Tuite <colmtuite@gmail.com>
2018-09-13 17:29:38 +0100

    Add overflow utilities to Box

--- src/components/Box.js
+++ src/components/Box.js
@@ -164,7 +164,20 @@ const Box = styled.div`
 
   ${p => p.alignmiddle && css` vertical-align: middle; `}
 
-  ${p => p.oyauto && css` overflow-y: auto; `}
+  ${p => p.overflow_visible && css` overflow: visible; `}
+  ${p => p.overflow_hidden && css` overflow: hidden; `}
+  ${p => p.overflow_scroll && css` overflow: scroll; `}
+  ${p => p.overflow_auto && css` overflow: auto; `}
+
+  ${p => p.ox_visible && css` overflow-x: visible; `}
+  ${p => p.ox_hidden && css` overflow-x: hidden; `}
+  ${p => p.ox_scroll && css` overflow-x: scroll; `}
+  ${p => p.ox_auto && css` overflow-x: auto; `}
+
+  ${p => p.oy_visible && css` overflow-x: visible; `}
+  ${p => p.oy_hidden && css` overflow-y: hidden; `}
+  ${p => p.oy_scroll && css` overflow-y: scroll; `}
+  ${p => p.oy_auto && css` overflow-y: auto; `}
 
   ${p => p.relative && css` position: relative; `}
 `;
