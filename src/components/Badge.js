98175573693dde1ed57cc80c4fbe7424a6f00b61
Colm Tuite <colmtuite@gmail.com>
2018-09-23 22:45:13 +0100

    Turns out inline flex on badge was a dumb idea

--- src/components/Badge.js
+++ src/components/Badge.js
@@ -3,7 +3,7 @@ import styled, { css } from 'styled-components';
 import * as theme from './../theme/';
 
 const Badge = styled.span`
-  display: flex;
+  display: inline-flex;
   align-items: center;
   font-family: ${theme.UNTITLEDSANSMEDIUM};
   font-size: ${theme.FONTSIZE_100};
