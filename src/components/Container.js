72aeec8de879a3493c556fa8cefc0d2035486034
Colm Tuite <colmtuite@gmail.com>
2018-09-29 09:51:02 +0100

    Fix syntax error

--- src/components/Container.js
+++ src/components/Container.js
@@ -21,61 +21,61 @@ const Container = styled.div`
   `}
 
   @media screen and (min-width: ${theme.BREAKPOINT_100}) {
-    ${props => props.bp1size1 && css`
+    ${props => props.bp1_size1 && css`
       max-width: 100%;
     `}
-    ${props => props.bp1size2 && css`
+    ${props => props.bp1_size2 && css`
       max-width: 65rem;
     `}
-    ${props => props.bp1size3 && css`
+    ${props => props.bp1_size3 && css`
       max-width: 45rem;
     `}
-    ${props => props.bp1size4 && css`
+    ${props => props.bp1_size4 && css`
       max-width: 25rem;
     `}
   }
 
   @media screen and (min-width: ${theme.BREAKPOINT_200}) {
-    ${props => props.bp2size1 && css`
+    ${props => props.bp2_size1 && css`
       max-width: 100%;
     `}
-    ${props => props.bp2size2 && css`
+    ${props => props.bp2_size2 && css`
       max-width: 65rem;
     `}
-    ${props => props.bp2size3 && css`
+    ${props => props.bp2_size3 && css`
       max-width: 45rem;
     `}
-    ${props => props.bp2size4 && css`
+    ${props => props.bp2_size4 && css`
       max-width: 25rem;
     `}
   }
 
   @media screen and (min-width: ${theme.BREAKPOINT_300}) {
-    ${props => props.bp3size1 && css`
+    ${props => props.bp3_size1 && css`
       max-width: 100%;
     `}
-    ${props => props.bp3size2 && css`
+    ${props => props.bp3_size2 && css`
       max-width: 65rem;
     `}
-    ${props => props.bp3size3 && css`
+    ${props => props.bp3_size3 && css`
       max-width: 45rem;
     `}
-    ${props => props.bp3size4 && css`
+    ${props => props.bp3_size4 && css`
       max-width: 25rem;
     `}
   }
 
   @media screen and (min-width: ${theme.BREAKPOINT_400}) {
-    ${props => props.bp4size1 && css`
+    ${props => props.bp4_size1 && css`
       max-width: 100%;
     `}
-    ${props => props.bp4size2 && css`
+    ${props => props.bp4_size2 && css`
       max-width: 65rem;
     `}
-    ${props => props.bp4size3 && css`
+    ${props => props.bp4_size3 && css`
       max-width: 45rem;
     `}
-    ${props => props.bp4size4 && css`
+    ${props => props.bp4_size4 && css`
       max-width: 25rem;
     `}
   }
