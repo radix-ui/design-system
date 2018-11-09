55c479b01e6c0a6b90621d7004c01e145cf7685e
Stephen Haney <stephen.haney@gmail.com>
2018-11-09 11:05:07 -0800

    ContextMenu delight

--- src/components/ContextMenu.js
+++ src/components/ContextMenu.js
@@ -2,12 +2,19 @@ import styled, { css } from 'styled-components';
 import * as theme from './../theme/';
 
 const ContextMenu = styled.nav`
+  display: flex;
+  flex-direction: column;
   background-color: ${theme.WHITE};
   box-shadow: 0 40px 80px hsla(208, 34%, 10%, 0.045), 0 15px 35px hsla(208, 44%, 10%, 0.045),
     0 3px 10px rgba(0, 0, 0, 0.08);
   border-radius: 4px;
   position: fixed;
   z-index: ${theme.ZINDEX_CONTEXT_MENU};
+
+  > div,
+  > li {
+    border-radius: 4px;
+  }
 
   ${props => props.top && css`
     top: ${props.top};
