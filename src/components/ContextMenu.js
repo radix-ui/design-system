f4280147dfb197bf2d24855b521528396883b7df
Stephen Haney <stephen.haney@gmail.com>
2018-11-09 11:09:31 -0800

    ContextMenu children get rounded corners

--- src/components/ContextMenu.js
+++ src/components/ContextMenu.js
@@ -1,19 +1,21 @@
 import styled, { css } from 'styled-components';
 import * as theme from './../theme/';
 
+const borderRadius = '4px';
 const ContextMenu = styled.nav`
   display: flex;
   flex-direction: column;
   background-color: ${theme.WHITE};
   box-shadow: 0 40px 80px hsla(208, 34%, 10%, 0.045), 0 15px 35px hsla(208, 44%, 10%, 0.045),
     0 3px 10px rgba(0, 0, 0, 0.08);
-  border-radius: 4px;
+  border-radius: ${borderRadius};
   position: fixed;
   z-index: ${theme.ZINDEX_CONTEXT_MENU};
 
   > div,
-  > li {
-    border-radius: 4px;
+  > li,
+  > button, {
+    border-radius: ${borderRadius};
   }
 
   ${props => props.top && css`
