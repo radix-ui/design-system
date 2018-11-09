4b4fc262a3047d557b4540c48039280225d934dd
Stephen Haney <stephen.haney@gmail.com>
2018-11-09 15:34:57 -0800

    Adding forceShow prop to HoverZonePanel

--- src/components/HoverZonePanel.js
+++ src/components/HoverZonePanel.js
@@ -1,11 +1,16 @@
 import styled, { css } from 'styled-components';
 import HoverZone from './HoverZone';
 
-const HoverZoneOpacity = styled.div`
+const HoverZonePanel = styled.div`
   opacity: 0;
+
   ${HoverZone}:hover & {
     opacity: 1;
   }
+
+  ${p => p.forceShow && css`
+    opacity: 1;
+  `}
 `;
 
-export default HoverZoneOpacity;
+export default HoverZonePanel;
