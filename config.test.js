7c6a948554ac7b679d3b2d59c9b982f40a34e076
elliotbonneville <elliot.bonneville@gmail.com>
2019-02-11 08:17:58 -0500

    Use sane name for tests

--- config.test.js
+++ config.test.js
@@ -3,5 +3,5 @@ import Adapter from 'enzyme-adapter-react-16';
 
 configure({ adapter: new Adapter() });
 
-let context = require.context('./src', true, /\.testable\.js$/);
+let context = require.context('./src', true, /\.test\.js$/);
 context.keys().forEach(context);
