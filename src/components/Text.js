6834da316b840667b526a7f22c69bc3e32218959
Pedro Duarte <contact@peduarte.com>
2019-03-29 17:47:21 +0000

    Add Text Component

---
+++ src/components/Text.js
@@ -0,0 +1,34 @@
+import styled from "styled-components";
+import {
+  color,
+  space,
+  fontSize,
+  fontFamily,
+  fontStyle,
+  textAlign,
+  lineHeight,
+  fontWeight,
+  letterSpacing,
+  themeGet,
+} from "styled-system";
+import { theme, themeColor } from "../theme";
+
+export const Text = styled.span`
+	font-family: ${themeGet("fonts.normal")};
+	font-size: ${themeGet("fontSizes.4")};
+	color: ${themeColor("grays.7")};
+	margin: 0;
+	padding: 0;
+
+	${color}
+	${space}
+	${fontSize}
+	${fontStyle}
+	${fontFamily}
+  ${textAlign}
+  ${lineHeight}
+  ${fontWeight}
+  ${letterSpacing}
+`;
+
+Text.defaultProps = { theme };
---
+++ src/components/Text.story.js
@@ -0,0 +1,77 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { Box } from "./Box";
+import { Text } from "./Text";
+
+storiesOf("Components|Text", module).add("default", () => (
+  <>
+    <Box mb="4">
+      <Text as="p" fontSize={0}>
+        Text
+      </Text>
+      <Text as="p" fontSize={1}>
+        Text
+      </Text>
+      <Text as="p" fontSize={2}>
+        Text
+      </Text>
+      <Text as="p" fontSize={3}>
+        Text
+      </Text>
+      <Text as="p" fontSize={4}>
+        Text
+      </Text>
+      <Text as="p" fontSize={5}>
+        Text
+      </Text>
+      <Text as="p" fontSize={6}>
+        Text
+      </Text>
+      <Text as="p" fontSize={7}>
+        Text
+      </Text>
+      <Text as="p" fontSize={8}>
+        Text
+      </Text>
+      <Text as="p" fontSize={9}>
+        Text
+      </Text>
+      <Text as="p" fontSize={10}>
+        Text
+      </Text>
+    </Box>
+
+    <Box mb="4">
+      <Text as="p" fontStyle="italic">
+        Closing the gap between designers and developers.
+      </Text>
+      <Text as="p" fontWeight="bold">
+        Closing the gap between designers and developers.
+      </Text>
+    </Box>
+
+    <Box mb="4">
+      <Text as="p">The visual code editor</Text>
+      <Text as="p" color="blue">
+        The visual code editor
+      </Text>
+      <Text as="p" color="red">
+        The visual code editor
+      </Text>
+      <Text as="p" color="green">
+        The visual code editor
+      </Text>
+      <Text as="p" color="gray">
+        The visual code editor
+      </Text>
+    </Box>
+
+    <Box mb="4">
+      <Text as="p" textAlign="center" padding="4">
+        Almost all design tools are optimised for illustration. Vector-based tools are great for
+        wireframing and exploration, but when it comes to designing interactive components, complex
+        layouts and stateful user interfaces—we need something more powerful.
+      </Text>
+    </Box>
+  </>
+));
