2c26504728cb498bdaf1a6c58375bf6f5a22324a
Pedro Duarte <contact@peduarte.com>
2019-04-15 12:49:28 +0100

    Add AspectRatio Component

---
+++ src/components/AspectRatio.story.js
@@ -0,0 +1,32 @@
+import React from "react";
+import { storiesOf } from "@storybook/react";
+import { Box } from "./Box";
+import { Flex } from "./Flex";
+import { Heading } from "./Heading";
+import { AspectRatio } from "./AspectRatio";
+
+const Label = ({ children }) => (
+  <Flex height="100%" width="100%" justifyContent="center" alignItems="center">
+    <Heading fontSize={4}>{children}</Heading>
+  </Flex>
+);
+
+storiesOf("Components|AspectRatio", module).add("default", () => (
+  <Flex>
+    <Box m={4} width={300}>
+      <AspectRatio bg="blue">
+        <Label>1 / 1 (default)</Label>
+      </AspectRatio>
+    </Box>
+    <Box m={4} width={300}>
+      <AspectRatio bg="blue" ratio={4 / 5}>
+        <Label>4 / 5</Label>
+      </AspectRatio>
+    </Box>
+    <Box m={4} width={300}>
+      <AspectRatio bg="blue" ratio={16 / 9}>
+        <Label>16 / 9</Label>
+      </AspectRatio>
+    </Box>
+  </Flex>
+));
