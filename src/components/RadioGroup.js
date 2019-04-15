5bcd58844ea06a13b7eacf33691a2c8b4b23afce
Pedro Duarte <contact@peduarte.com>
2019-04-15 22:38:28 +0100

    Refactor Radio Component

--- src/components/RadioGroup.js
+++ src/components/RadioGroup.js
@@ -1,5 +1,7 @@
 import React from "react";
 import styled from "styled-components";
+import { space, themeGet } from "styled-system";
+import { theme, themeColor } from "../theme";
 
 export function RadioGroup({ as: Component = "div", children, value, onChange, name, ...props }) {
   const isControlled = Boolean(value);
@@ -17,13 +19,85 @@ export function RadioGroup({ as: Component = "div", children, value, onChange, n
   );
 }
 
-export const BaseRadio = styled.input``;
+const RadioWrapper = styled.label`
+  position: relative;
 
-export function Radio({ children, ...props }) {
+  ${space}
+`;
+
+RadioWrapper.defaultProps = { theme };
+
+const Input = styled.input`
+  appearance: none;
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  z-index: 0;
+  outline: none;
+  margin: 0;
+`;
+
+const TextWrapper = styled.span`
+  line-height: ${themeGet("space.5")};
+  font-family: ${themeGet("fonts.normal")};
+  font-size: ${themeGet("fontSizes.2")};
+  margin-left: ${themeGet("space.1")};
+  margin-right: ${themeGet("space.3")};
+`;
+
+TextWrapper.defaultProps = { theme };
+
+const FakeRadio = styled.div`
+  width: ${themeGet("space.4")};
+  height: ${themeGet("space.4")};
+  border-radius: 50%;
+  box-shadow: inset 0 0 0 1px ${themeColor("grays.3")};
+  display: inline-flex;
+  align-items: center;
+  justify-content: center;
+  color: transparent;
+
+  &::before {
+    content: "";
+    width: 50%;
+    height: 50%;
+    color: inherit;
+    border-radius: inherit;
+    background-color: currentColor;
+  }
+
+  input:checked + & {
+    color: ${themeColor("blues.4")};
+  }
+
+  input:hover + & {
+    box-shadow: inset 0 0 0 1px ${themeColor("grays.4")};
+  }
+
+  input:focus + & {
+    box-shadow: inset 0 0 0 1px ${themeColor("blues.4")};
+  }
+`;
+
+FakeRadio.defaultProps = { theme };
+
+export function Radio({ children, value, defaultChecked, checked, onChange, name, ...props }) {
   return (
-    <label>
-      <BaseRadio {...props} type="radio" />
-      {children}
-    </label>
+    <RadioWrapper {...props}>
+      <Input
+        type="radio"
+        value={value}
+        onChange={onChange}
+        defaultChecked={defaultChecked}
+        checked={checked}
+        name={name}
+      />
+      <FakeRadio />
+      {children && <TextWrapper>{children}</TextWrapper>}
+    </RadioWrapper>
   );
 }
+
+Radio.defaultProps = { theme };
--- src/components/RadioGroup.story.js
+++ src/components/RadioGroup.story.js
@@ -19,6 +19,14 @@ storiesOf("Components|RadioGroup", module).add("default", () => (
     </Box>
 
     <Box mb="4">
+      <Text as="p">Uncontrolled and without children</Text>
+      <RadioGroup name="flexDirection">
+        <Radio value="row" defaultChecked />
+        <Radio value="column" />
+      </RadioGroup>
+    </Box>
+
+    <Box mb="4">
       <Text as="p">Controlled</Text>
       <Component initialState={{ value: "wrap" }}>
         {({ state, setState }) => (
@@ -28,8 +36,12 @@ storiesOf("Components|RadioGroup", module).add("default", () => (
               value={state.value}
               onChange={event => setState({ value: event.target.value })}
             >
-              <Radio value="wrap">Wrap</Radio>
-              <Radio value="nowrap">Nowrap</Radio>
+              <Radio ml={0} value="wrap">
+                Wrap
+              </Radio>
+              <Radio ml={0} value="nowrap">
+                Nowrap
+              </Radio>
             </RadioGroup>
             <Button my="3" onClick={() => setState({ value: "nowrap" })}>
               select nowrap
