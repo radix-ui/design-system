0d74277dabd34016d597c3b96dc4612d1d82c401
Pedro Duarte <contact@peduarte.com>
2019-04-16 11:26:40 +0100

    Remove styled-components macro from Button

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,4 +1,4 @@
-import styled, { css, keyframes } from "styled-components/macro";
+import styled, { css, keyframes } from "styled-components";
 import { theme, themeColor } from "../theme";
 import { space, buttonStyle, themeGet, variant } from "styled-system";
 
