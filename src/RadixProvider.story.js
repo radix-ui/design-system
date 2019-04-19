bf08985f6a7648f0fa6c32ddbbb793a2ced859ac
Pedro Duarte <contact@peduarte.com>
2019-04-19 21:22:10 +0100

    Typescale theme story

--- src/RadixProvider.story.js
+++ src/RadixProvider.story.js
@@ -1,9 +1,12 @@
 import React from 'react';
+import Component from 'react-component-component';
+import { modularScale } from 'polished';
 import { storiesOf } from '@storybook/react';
 import { RadixProvider } from './RadixProvider';
 import { theme } from './theme';
 import { Box } from './components/Box';
-import { themeGet } from 'styled-system';
+import { Flex } from './components/Flex';
+import { Text } from './components/Text';
 import { Heading } from './components/Heading';
 
 const colorfulTheme = {
@@ -21,14 +24,102 @@ const colorfulTheme = {
   },
 };
 
-colorfulTheme.buttons = {
-  benoit: {
-    background: themeGet('colors.blue')({ theme: colorfulTheme }),
-    color: themeGet('colors.blues.1')({ theme: colorfulTheme }),
-  },
-};
+storiesOf('Theme|RadixProvider', module).add('typescale', () => (
+  <Component initialState={{ theme }}>
+    {({ state, setState }) => {
+      const makeScale = scale => {
+        return {
+          ...theme,
+          fontSizes: [-4, -3, -2, -1, 0, 1, 2, 3, 4, 5, 6].map(n => {
+            return modularScale(n, '1rem', scale);
+          }),
+        };
+      };
 
-storiesOf('RadixProvider', module).add('default', () => (
+      const setScale = e => setState({ theme: makeScale(e.target.value) });
+
+      return (
+        <RadixProvider theme={state.theme}>
+          <>
+            <Flex
+              flexDirection="column"
+              textAlign="center"
+              alignItems="flex-start"
+              style={{ position: 'fixed', right: 20, top: 20 }}
+            >
+              <Box mb={2}>Type scales</Box>
+              <label>
+                <input
+                  type="radio"
+                  name="scale"
+                  value="default"
+                  defaultChecked
+                  onChange={() => setState({ theme })}
+                />
+                default
+              </label>
+              {[
+                'minorSecond',
+                'majorThird',
+                'goldenSection',
+                'minorSixth',
+                'perfectFifth',
+              ].map(scale => (
+                <label>
+                  <input
+                    key={scale}
+                    type="radio"
+                    name="scale"
+                    value={scale}
+                    onChange={setScale}
+                  />
+                  {scale}
+                </label>
+              ))}
+            </Flex>
+            <Box>
+              <Text as="p" fontSize={0}>
+                Radix
+              </Text>
+              <Text as="p" fontSize={1}>
+                Radix
+              </Text>
+              <Text as="p" fontSize={2}>
+                Radix
+              </Text>
+              <Text as="p" fontSize={3}>
+                Radix
+              </Text>
+              <Text as="p" fontSize={4}>
+                Radix
+              </Text>
+              <Text as="p" fontSize={5}>
+                Radix
+              </Text>
+              <Text as="p" fontSize={6}>
+                Radix
+              </Text>
+              <Text as="p" fontSize={7}>
+                Radix
+              </Text>
+              <Text as="p" fontSize={8}>
+                Radix
+              </Text>
+              <Text as="p" fontSize={9}>
+                Radix
+              </Text>
+              <Text as="p" fontSize={10}>
+                Radix
+              </Text>
+            </Box>
+          </>
+        </RadixProvider>
+      );
+    }}
+  </Component>
+));
+
+storiesOf('Theme|RadixProvider', module).add('colourful', () => (
   <RadixProvider theme={colorfulTheme}>
     <>
       <Box>
