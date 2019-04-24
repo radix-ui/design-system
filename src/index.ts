42d284cbe2c0b0674d3e9ae0e0d1ac4a5994c174
Pedro Duarte <contact@peduarte.com>
2019-04-24 16:00:14 +0100

    Export all components

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,20 @@
+export { AspectRatio } from './components/AspectRatio';
 export { Box } from './components/Box';
 export { Button } from './components/Button';
 export { Card } from './components/Card';
+export { Checkbox } from './components/Checkbox';
 export { Container } from './components/Container';
+export { Dialog } from './components/Dialog';
 export { Flex } from './components/Flex';
+export { GhostButton } from './components/GhostButton';
+export { Heading } from './components/Heading';
 export { Input } from './components/Input';
+export { RadioGroup, Radio } from './components/RadioGroup';
+export { Select } from './components/Select';
+export { Slider } from './components/Slider';
 export { Text } from './components/Text';
+export { Textarea } from './components/Textarea';
+export { ToggleButtonGroup, ToggleButton } from './components/ToggleButton';
+export { Tooltip } from './components/Tooltip';
 
 export { theme } from './theme';
