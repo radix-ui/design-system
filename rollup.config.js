718540919445b338dfe8733c7174bde440be1b74
Pedro Duarte <contact@peduarte.com>
2019-04-25 11:35:38 +0100

    Authoring test

--- rollup.config.js
+++ rollup.config.js
@@ -1,4 +1,5 @@
 import typescript from 'rollup-plugin-typescript2';
+import url from 'rollup-plugin-url';
 import pkg from './package.json';
 
 export default {
@@ -16,9 +17,15 @@ export default {
   external: [
     ...Object.keys(pkg.dependencies || {}),
     ...Object.keys(pkg.peerDependencies || {}),
+    '@reach/dialog/styles.css',
   ],
   plugins: [
+    url({
+      include: ['**/*.woff', '**/*.woff2'],
+    }),
     typescript({
+      clean: true,
+      abortOnError: false,
       typescript: require('typescript'),
     }),
   ],
