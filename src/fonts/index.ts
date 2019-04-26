575c1f5adc9c3f3c81b12935a2b39ab6f15a706a
Pedro Duarte <contact@peduarte.com>
2019-04-26 10:53:27 +0100

    Update readme and format

--- src/fonts/index.ts
+++ src/fonts/index.ts
@@ -1,8 +1,8 @@
-export { default as UntitledSansWoff } from "./UntitledSansWeb-Regular.woff";
-export { default as UntitledSansWoff2 } from "./UntitledSansWeb-Regular.woff2";
+export { default as UntitledSansWoff } from './UntitledSansWeb-Regular.woff';
+export { default as UntitledSansWoff2 } from './UntitledSansWeb-Regular.woff2';
 export {
-  default as UntitledSansMediumWoff
-} from "./UntitledSansWeb-Medium.woff";
+  default as UntitledSansMediumWoff,
+} from './UntitledSansWeb-Medium.woff';
 export {
-  default as UntitledSansMediumWoff2
-} from "./UntitledSansWeb-Medium.woff2";
+  default as UntitledSansMediumWoff2,
+} from './UntitledSansWeb-Medium.woff2';
