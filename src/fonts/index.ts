9e6463a84a63229a655a0b98ad6d67baddf767e2
Pedro Duarte <contact@peduarte.com>
2019-04-26 09:41:50 +0100

    add fonts

---
+++ src/fonts/index.ts
@@ -0,0 +1,8 @@
+export { default as UntitledSansWoff } from "./UntitledSansWeb-Regular.woff";
+export { default as UntitledSansWoff2 } from "./UntitledSansWeb-Regular.woff2";
+export {
+  default as UntitledSansMediumWoff
+} from "./UntitledSansWeb-Medium.woff";
+export {
+  default as UntitledSansMediumWoff2
+} from "./UntitledSansWeb-Medium.woff2";
