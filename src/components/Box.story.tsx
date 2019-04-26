6fef2ccea4dc6efe3eb8f714d81f35d8b8ffaaf8
Pedro Duarte <contact@peduarte.com>
2019-04-26 08:59:22 +0100

    Fix Storybook

--- src/components/Box.story.tsx
+++ src/components/Box.story.tsx
@@ -4,8 +4,8 @@ import { Box } from './Box';
 
 storiesOf('Components|Box', module).add('default', () => (
   <>
-    <Box mb={4} bg="red">
-      <Box width={120} height={120} bg="black" />
+    <Box mb={4}>
+      <Box width={120} height={120} bg="blue" />
     </Box>
     <Box mb={4}>
       <Box p="4" bg="black">
