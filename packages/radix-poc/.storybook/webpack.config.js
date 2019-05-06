33c0bae31edf2a7d5d4ea6453cf5de25b93a2370
Pedro Duarte <contact@peduarte.com>
2019-05-06 16:16:58 +0100

    Storybook build

--- packages/radix-poc/.storybook/webpack.config.js
+++ packages/radix-poc/.storybook/webpack.config.js
@@ -1,7 +1,10 @@
+const path = require('path');
+
 module.exports = ({ config, mode }) => {
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     loader: require.resolve('babel-loader'),
+    include: path.resolve(__dirname, '../src'),
     options: {
       presets: [['react-app', { flow: false, typescript: true }]],
     },
