9470f1f5a3b5c483294475ab30af19e006ced159
Pedro Duarte <contact@peduarte.com>
2019-05-06 23:22:25 +0100

    Testing mdx live

--- packages/website/gatsby-config.js
+++ packages/website/gatsby-config.js
@@ -1,3 +1,21 @@
 module.exports = {
-  plugins: ['gatsby-plugin-styled-components'],
+  plugins: [
+    'gatsby-plugin-styled-components',
+    {
+      resolve: 'gatsby-mdx',
+      options: {
+        extensions: ['.md', '.mdx'],
+        defaultLayouts: {
+          default: require.resolve('./src/components/Layout'),
+        },
+      },
+    },
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'pages',
+        path: `${__dirname}/src/pages/`,
+      },
+    },
+  ],
 };
--- packages/website/src/components/Layout.js
+++ packages/website/src/components/Layout.js
@@ -1,8 +1,23 @@
 import React from 'react';
 import { RadixProvider } from 'radix-poc';
+import { MDXProvider } from '@mdx-js/react';
+import CodeBlock from './CodeBlock';
+// import { Button } from 'radix-poc';
+
+const components = {
+  pre: props => <div {...props} />,
+  code: CodeBlock,
+  // h1: props => <h1 style={{ color: 'tomato' }} {...props} />,
+  // button: Button,
+  button: props => <div {...props} />,
+};
 
 function Layout({ children }) {
-  return <RadixProvider>{children}</RadixProvider>;
+  return (
+    <RadixProvider>
+      <MDXProvider components={components}>{children}</MDXProvider>
+    </RadixProvider>
+  );
 }
 
 export default Layout;
