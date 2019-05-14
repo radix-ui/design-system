35cc800fbdd6bc826b0e091b7c0660028f27e35e
Pedro Duarte <contact@peduarte.com>
2019-05-14 13:13:26 +0200

    radix: update Code component

--- packages/radix-poc/src/components/Code.tsx
+++ packages/radix-poc/src/components/Code.tsx
@@ -1,19 +1,23 @@
 import styled from '@emotion/styled';
 import css from '@styled-system/css';
+import { space, SpaceProps } from 'styled-system';
 import { themeGet } from 'styled-system';
 
-export const Code = styled('code')(props =>
-  css({
-    backgroundColor: 'blues.1',
-    color: 'blue',
-    borderColor: 'grays.3',
-    borderRadius: 1,
-    fontFamily: 'mono',
-    fontSize: '0.75em',
-    letterSpacing: 0,
-    lineHeight: 'inherit',
-    paddingY: `calc(${themeGet('space.1')(props)} / 2)`,
-    paddingX: 1,
-    whiteSpace: 'nowrap',
-  })
+export const Code = styled('code')<SpaceProps>(
+  props =>
+    css({
+      display: 'inline-block',
+      backgroundColor: 'blues.1',
+      color: 'blue',
+      borderColor: 'grays.3',
+      borderRadius: 1,
+      fontFamily: 'mono',
+      fontSize: '0.75em',
+      letterSpacing: 0,
+      lineHeight: 'inherit',
+      paddingY: `calc(${themeGet('space.1')(props)} / 2)`,
+      paddingX: 1,
+      whiteSpace: 'nowrap',
+    }),
+  space
 );
