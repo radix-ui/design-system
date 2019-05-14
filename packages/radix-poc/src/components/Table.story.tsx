cb004b1daf658702d654539f0962fc261a100866
Pedro Duarte <contact@peduarte.com>
2019-05-14 10:16:49 +0200

    radix: add Table Component

---
+++ packages/radix-poc/src/components/Table.story.tsx
@@ -0,0 +1,33 @@
+import React from 'react';
+import { storiesOf } from '@storybook/react';
+import { Box } from './Box';
+import { Table, Thead, Tbody, Tr, Th, Td } from './Table';
+
+storiesOf('Components|Table', module).add('default', () => (
+  <Box maxWidth="300px">
+    <Table>
+      <Thead>
+        <Tr>
+          <Th>Name</Th>
+          <Th>Type</Th>
+          <Th>Default</Th>
+          <Th>Description</Th>
+        </Tr>
+      </Thead>
+      <Tbody>
+        <Tr>
+          <Td>size</Td>
+          <Td>boolean</Td>
+          <Td>3</Td>
+          <Td>font size</Td>
+        </Tr>
+        <Tr>
+          <Td>size</Td>
+          <Td>boolean</Td>
+          <Td>3</Td>
+          <Td>font size</Td>
+        </Tr>
+      </Tbody>
+    </Table>
+  </Box>
+));
---
+++ packages/radix-poc/src/components/Table.tsx
@@ -0,0 +1,27 @@
+import styled from '@emotion/styled';
+import css from '@styled-system/css';
+
+export const Table = styled('table')({
+  width: '100%',
+  borderCollapse: 'collapse',
+  textAlign: 'left',
+});
+export const Thead = styled('thead')({});
+export const Tbody = styled('tbody')({});
+export const Tr = styled('tr')({});
+export const Td = styled('td')(
+  css({
+    paddingY: 3,
+    paddingRight: 3,
+    borderBottom: '1px solid',
+    borderColor: 'grays.3',
+  })
+);
+export const Th = styled(Td)(
+  css({
+    fontFamily: 'medium',
+    fontWeight: 500,
+    borderWidth: 2,
+  })
+).withComponent('th');
+export const Tfoot = styled('tfoot')({});
