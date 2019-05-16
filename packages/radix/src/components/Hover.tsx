d2686d59544150b4872f27aa4524f8d2fd36dbb3
Pedro Duarte <contact@peduarte.com>
2019-05-16 17:21:48 +0200

    radix: add `as` prop to `Hover`

--- packages/radix/src/components/Hover.tsx
+++ packages/radix/src/components/Hover.tsx
@@ -3,18 +3,23 @@ import { Box, BoxProps } from './Box';
 
 type HoverProps = BoxProps & {
   children(isHovered: boolean): JSX.Element;
+  as?: any;
 };
 
-export const Hover: FC<HoverProps> = ({ children, ...props }) => {
+export const Hover: FC<HoverProps> = ({
+  as: Component = Box,
+  children,
+  ...props
+}) => {
   const [isHovered, setIsHovered] = useState(false);
 
   return (
-    <Box
+    <Component
       {...props}
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
       {children(isHovered)}
-    </Box>
+    </Component>
   );
 };
