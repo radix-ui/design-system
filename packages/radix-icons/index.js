a8aaed35aa5369b97639c65e777c92b17cb3e6fe
Steven Benisek <steven.benisek@gmail.com>
2019-06-24 16:23:04 +0200

    chore(radix-icons): use rollup
    - use rollup with typescript plugin
    - align package main and module with radix (avoids declaration files in
    the root)

--- packages/radix-icons/index.js
+++
@@ -1,2 +0,0 @@
-module.exports = require('./dist/index.js');
-module.exports.__esModule = true;
---
+++ packages/radix-icons/rollup.config.js
@@ -0,0 +1,26 @@
+import typescript from 'rollup-plugin-typescript2';
+import pkg from './package.json';
+
+export default {
+  input: 'src/index.tsx',
+  output: [
+    {
+      file: pkg.main,
+      format: 'cjs',
+    },
+    {
+      file: pkg.module,
+      format: 'es',
+    },
+  ],
+  external: [
+    ...Object.keys(pkg.dependencies || {}),
+    ...Object.keys(pkg.peerDependencies || {}),
+  ],
+  plugins: [
+    typescript({
+      clean: true,
+      typescript: require('typescript'),
+    }),
+  ],
+};
