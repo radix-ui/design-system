f00f3a17d876bdeb57384653743566023ce8493e
Pedro Duarte <contact@peduarte.com>
2019-06-25 14:46:09 +0100

    feat(radix): expose minHeight and padding to `MenuItem` Component (#140)

--- packages/website/src/components/NavItem.js
+++ packages/website/src/components/NavItem.js
@@ -4,7 +4,7 @@ import { MenuItem, Text } from '@modulz/radix';
 
 function NavItem({ label, active, ...props }) {
   return (
-    <MenuItem as={Link} variant={active ? 'active' : undefined} {...props}>
+    <MenuItem {...props} as={Link} variant={active ? 'active' : undefined} px={5} minHeight={6}>
       <Text size={2} textColor={active ? 'white' : undefined}>
         {label}
       </Text>
