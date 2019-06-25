c799ffb7077b430d426e2453798df3117ff88a3c
Aarón García Hervás <aar.garc.herv@gmail.com>
2019-06-26 00:27:21 +0200

    feat(radix-system): expose fontWeight

---
+++ packages/radix-system/src/system/fontWeight.ts
@@ -0,0 +1,11 @@
+import * as CSS from 'csstype';
+import { system, Config } from '@styled-system/core';
+import { Prop } from '../utils/types';
+
+export interface FontWeightProps {
+  fontWeight?: Prop<CSS.FontWeightProperty>;
+}
+
+const config: Config = { fontWeight: true };
+
+export const fontWeight = system(config);
