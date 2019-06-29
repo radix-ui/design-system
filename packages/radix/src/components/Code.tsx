4b290322299341017d268be7c7c817cc42c5536f
Pedro Duarte <contact@peduarte.com>
2019-06-29 20:18:34 +0100

    feat(radix): update mono font to Operator Mono (#143)
    * feat(radix): add PitchSansMedium Font
    
    * feat(radix): implement semibold
    
    * style: update mono font to Operator Mono
    
    * docs: update getting started

--- packages/radix/src/components/Code.tsx
+++ packages/radix/src/components/Code.tsx
@@ -1,22 +1,20 @@
 import styled from 'styled-components';
 import css from '@styled-system/css';
 import { margin, MarginProps } from '@modulz/radix-system';
-import { themeGet } from '@styled-system/theme-get';
 
 export const Code = styled('code')<MarginProps>(
-  props =>
-    css({
-      display: 'inline-block',
-      backgroundColor: 'blues.1',
-      color: 'blues.5',
-      borderRadius: 1,
-      fontFamily: 'mono',
-      fontSize: '0.75em',
-      letterSpacing: 0,
-      lineHeight: 'inherit',
-      paddingY: `calc(${themeGet('space.1')(props)} / 2)`,
-      paddingX: 1,
-      whiteSpace: 'nowrap',
-    }),
+  css({
+    display: 'inline-block',
+    backgroundColor: 'blues.1',
+    color: 'blues.5',
+    borderRadius: 1,
+    fontFamily: 'mono',
+    fontSize: 2,
+    letterSpacing: '-0.0175em',
+    lineHeight: 'inherit',
+    paddingY: '0.15em',
+    paddingX: 1,
+    whiteSpace: 'nowrap',
+  }),
   margin
 );
--- packages/radix/src/fonts/index.ts
+++ packages/radix/src/fonts/index.ts
@@ -2,3 +2,5 @@ export { default as UntitledSansWoff } from './UntitledSansWeb-Regular.woff';
 export { default as UntitledSansWoff2 } from './UntitledSansWeb-Regular.woff2';
 export { default as UntitledSansMediumWoff } from './UntitledSansWeb-Medium.woff';
 export { default as UntitledSansMediumWoff2 } from './UntitledSansWeb-Medium.woff2';
+export { default as OperatorMonoBookWoff } from './OperatorMono-Book.woff';
+export { default as OperatorMonoBookWoff2 } from './OperatorMono-Book.woff2';
