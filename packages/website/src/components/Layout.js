78dfacc42a8f2d5f155b0bba3dec184e03eefc77
Pedro Duarte <contact@peduarte.com>
2019-06-29 10:31:03 +0100

    feat(website): add social meta tags (#146)

--- packages/website/src/components/Layout.js
+++ packages/website/src/components/Layout.js
@@ -1,5 +1,29 @@
+import React from 'react';
+import Helmet from 'react-helmet';
+
 function Layout({ children, ...props }) {
-  return children;
+  return (
+    <>
+      <Helmet>
+        <title>Radix</title>
+        <link rel="icon" type="image/png" href="/favicon.png" />
+
+        <meta property="og:url" content="https://radix.modulz.app/" />
+        <meta property="og:image" content="https://radix.modulz.app/social/hero.png" />
+        <meta property="og:title" content="Radix" />
+        <meta property="og:description" content="Design System and Component Library for Modulz" />
+
+        <meta name="twitter:url" content="https://radix.modulz.app/" />
+        <meta name="twitter:image" content="https://radix.modulz.app/social/hero.png" />
+        <meta name="twitter:title" content="Radix" />
+        <meta name="twitter:description" content="Design System and Component Library for Modulz" />
+        <meta name="twitter:site" content="@modulz" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:creator" content="@modulz" />
+      </Helmet>
+      {children}
+    </>
+  );
 }
 
 export default Layout;
