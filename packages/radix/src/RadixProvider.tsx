299c1e1e1fe97f06394bcebb37487a2b9df01323
Pedro Duarte <contact@peduarte.com>
2019-07-01 17:29:11 +0100

    test: remove fonts

--- packages/radix/src/RadixProvider.tsx
+++ packages/radix/src/RadixProvider.tsx
@@ -2,7 +2,6 @@ import React, { FC } from 'react';
 import { ThemeProvider } from 'styled-components';
 import { theme as defaultTheme } from './theme';
 import { GlobalStyles } from './GlobalStyles';
-import { FontStyles } from './fonts';
 
 type RadixProviderProps = {
   // TODO: type Theme
@@ -15,7 +14,6 @@ export const RadixProvider: FC<RadixProviderProps> = ({ theme = defaultTheme, ch
       <>
         <GlobalStyles />
         {children}
-        <FontStyles />
       </>
     </ThemeProvider>
   );
