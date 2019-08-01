7f8c0b03b13a4631ae0b19385ee825b83f5ae4bd
Pedro Duarte <contact@peduarte.com>
2019-08-01 13:07:26 +0100

    chore: wire up with yalc

--- packages/radix/.storybook/webpack.config.js
+++ packages/radix/.storybook/webpack.config.js
@@ -1,6 +1,15 @@
 const path = require('path');
 
 module.exports = ({ config, mode }) => {
+  config.watchOptions = {
+    ...config.watchOptions,
+    ignored: [
+      // ignore all `node_modules` except `@modulz/primitives`
+      /node_modules([\\]+|\/)+(?!@modulz\/primitives)/,
+      // ignore all `node_modules` inside `@modulz/primitives`
+      /\@modulz\/primitives([\\]+|\/)node_modules/,
+    ],
+  };
   config.module.rules.push({
     test: /\.(ts|tsx)$/,
     loader: require.resolve('babel-loader'),
