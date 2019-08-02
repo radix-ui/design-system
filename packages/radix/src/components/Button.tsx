417403776b7eaa292cb02fe8ec0dfe6e80e35349
Pedro Duarte <contact@peduarte.com>
2019-08-02 12:21:18 +0100

    refactor: comsume Button, Link, Divider and Link Primitives

--- packages/radix/src/components/Button.tsx
+++ packages/radix/src/components/Button.tsx
@@ -1,187 +1,184 @@
-import styled, { css as _css, keyframes } from 'styled-components';
+import React from 'react';
+import { css as _css, keyframes, withTheme } from 'styled-components';
 import css from '@styled-system/css';
 import themeGet from '@styled-system/theme-get';
-import { margin, MarginProps, variant, Prop } from '@modulz/radix-system';
+import { variant, Prop } from '@modulz/radix-system';
+import { Button as ButtonPrimitive, ButtonProps as ButtonPrimitiveProps } from '@modulz/primitives';
 
-const waitingAnimation = (props: any) => keyframes`
+const waitingAnimation = (props: ButtonProps) => keyframes`
   100% {
-    transform: translateX(${themeGet(props.size === 'large' ? 'space.9' : 'space.7')(props)});
+    transform: translateX(${themeGet(props.size === 1 ? 'space.9' : 'space.7')(props)});
 	}
 `;
 
 type Variants = 'gray' | 'blue' | 'green' | 'red' | 'active' | 'waiting';
 type Sizes = 0 | 1;
 
-type ButtonProps = MarginProps & {
+type ButtonProps = {
   variant?: Prop<Variants>;
   size?: Prop<Sizes>;
   // TODO: type Theme
   theme?: any;
-};
+} & ButtonPrimitiveProps;
 
-export const Button = styled('button')<ButtonProps>(
-  css({
-    alignItems: 'center',
-    appearance: 'none',
-    border: '1px solid',
-    borderRadius: 1,
-    cursor: 'pointer',
-    display: 'inline-flex',
-    fontFamily: 'medium',
-    fontWeight: 500,
-    outline: 'none',
-    paddingY: 0,
-    position: 'relative',
-    textAlign: 'center',
-    textDecoration: 'none',
-    userSelect: 'none',
-    whiteSpace: 'nowrap',
-    WebkitTapHighlightColor: 'rgba(0, 0, 0, 0)',
-    '&:focus': {
-      '&::-moz-focus-inner': {
-        border: 0,
-      },
-    },
-    '&:active': {
-      backgroundColor: 'grays.1',
-      borderColor: 'grays.4',
-    },
-    '&:disabled': {
-      backgroundColor: 'grays.1',
-      borderColor: 'grays.2',
-      color: 'grays.4',
-      cursor: 'not-allowed',
-    },
-  }),
-  variant({
-    variant: {
-      gray: {
-        backgroundColor: 'grays.0',
-        borderColor: 'grays.3',
-        color: 'grays.5',
-        '&:hover': {
-          borderColor: 'grays.4',
-        },
-      },
-      blue: {
-        backgroundColor: 'blues.0',
-        borderColor: 'blues.3',
-        color: 'blues.5',
-        '&:hover': {
-          borderColor: 'blues.4',
-        },
+export const Button = withTheme((props: ButtonProps) => (
+  <ButtonPrimitive
+    {...props}
+    css={[
+      css({
+        border: '1px solid',
+        borderRadius: 1,
+        cursor: 'pointer',
+        fontFamily: 'medium',
+        fontWeight: 500,
+        outline: 'none',
+        paddingY: 0,
+        position: 'relative',
+        textAlign: 'center',
+        textDecoration: 'none',
+        userSelect: 'none',
+        whiteSpace: 'nowrap',
+        WebkitTapHighlightColor: 'rgba(0, 0, 0, 0)',
         '&:focus': {
-          borderColor: 'blues.4',
-        },
-        '&:active': {
-          backgroundColor: 'blues.1',
-          borderColor: 'blues.4',
-        },
-      },
-      green: {
-        backgroundColor: 'greens.0',
-        borderColor: 'greens.3',
-        color: 'greens.5',
-        '&:hover': {
-          borderColor: 'greens.4',
-        },
-        '&:focus': {
-          borderColor: 'greens.4',
-        },
-        '&:active': {
-          backgroundColor: 'greens.1',
-          borderColor: 'greens.4',
-        },
-      },
-      red: {
-        backgroundColor: 'grays.0',
-        color: 'reds.5',
-        borderColor: 'grays.3',
-        '&:hover': {
-          borderColor: 'grays.4',
-        },
-      },
-      active: {
-        backgroundColor: 'grays.1',
-        borderColor: 'grays.3',
-        color: 'grays.5',
-        '&:hover': {
-          borderColor: 'grays.4',
-        },
-        '&:focus': {
-          borderColor: 'grays.4',
+          '&::-moz-focus-inner': {
+            border: 0,
+          },
         },
         '&:active': {
           backgroundColor: 'grays.1',
           borderColor: 'grays.4',
         },
-      },
-      waiting: {
-        backgroundColor: 'grays.2',
-        borderColor: 'grays.3',
-        color: 'transparent',
-      },
-    },
-  }),
-  variant({
-    size: {
-      0: {
-        fontSize: 2,
-        paddingX: 2,
-        height: 5,
-        minWidth: 5,
-      },
-      1: {
-        fontSize: 3,
-        paddingX: 3,
-        height: 6,
-        minWidth: 6,
-      },
-    },
-  }),
-  // Code below specifically for the `waiting` variant
-  ({ variant, theme, size }) =>
-    variant === 'waiting' &&
-    css({
-      overflow: 'hidden',
-      position: 'relative',
-      pointerEvents: 'none',
-      '&::before': {
-        position: 'absolute',
-        content: `''`,
-        top: 0,
-        left: '-100%',
-        width: '200%',
-        height: '100%',
-        backgroundImage: `linear-gradient(
+        '&:disabled': {
+          backgroundColor: 'grays.1',
+          borderColor: 'grays.2',
+          color: 'grays.4',
+          cursor: 'not-allowed',
+        },
+      }),
+      variant({
+        variant: {
+          gray: {
+            backgroundColor: 'grays.0',
+            borderColor: 'grays.3',
+            color: 'grays.5',
+            '&:hover': {
+              borderColor: 'grays.4',
+            },
+            '&:focus': {
+              borderColor: 'grays.4',
+            },
+          },
+          blue: {
+            backgroundColor: 'blues.0',
+            borderColor: 'blues.3',
+            color: 'blues.5',
+            '&:hover': {
+              borderColor: 'blues.4',
+            },
+            '&:focus': {
+              borderColor: 'blues.4',
+            },
+            '&:active': {
+              backgroundColor: 'blues.1',
+              borderColor: 'blues.4',
+            },
+          },
+          green: {
+            backgroundColor: 'greens.0',
+            borderColor: 'greens.3',
+            color: 'greens.5',
+            '&:hover': {
+              borderColor: 'greens.4',
+            },
+            '&:focus': {
+              borderColor: 'greens.4',
+            },
+            '&:active': {
+              backgroundColor: 'greens.1',
+              borderColor: 'greens.4',
+            },
+          },
+          red: {
+            backgroundColor: 'grays.0',
+            color: 'reds.5',
+            borderColor: 'grays.3',
+            '&:hover': {
+              borderColor: 'grays.4',
+            },
+          },
+          active: {
+            backgroundColor: 'grays.1',
+            borderColor: 'grays.3',
+            color: 'grays.5',
+            '&:hover': {
+              borderColor: 'grays.4',
+            },
+            '&:focus': {
+              borderColor: 'grays.4',
+            },
+            '&:active': {
+              backgroundColor: 'grays.1',
+              borderColor: 'grays.4',
+            },
+          },
+          waiting: {
+            backgroundColor: 'grays.2',
+            borderColor: 'grays.3',
+            color: 'transparent',
+          },
+        },
+      }),
+      variant({
+        size: {
+          0: {
+            fontSize: 2,
+            paddingX: 2,
+            height: 5,
+            minWidth: 5,
+          },
+          1: {
+            fontSize: 3,
+            paddingX: 3,
+            height: 6,
+            minWidth: 6,
+          },
+        },
+      }),
+      // Code below specifically for the `waiting` variant
+      props.variant === 'waiting' &&
+        css({
+          overflow: 'hidden',
+          position: 'relative',
+          pointerEvents: 'none',
+          '&::before': {
+            position: 'absolute',
+            content: `''`,
+            top: 0,
+            left: '-100%',
+            width: '200%',
+            height: '100%',
+            backgroundImage: `linear-gradient(
             -45deg,
             transparent 33%,
             rgba(0, 0, 0, 0.05) 33%,
             rgba(0, 0, 0, 0.05) 66%,
             transparent 66%
           )`,
-        backgroundSize:
-          size === 1
-            ? `${themeGet('space.9')({ theme })} ${themeGet('space.6')({
-                theme,
-              })}`
-            : `${themeGet('space.7')({ theme })} ${themeGet('space.5')({
-                theme,
-              })}`,
-      },
-    }),
-  _css`
+            backgroundSize:
+              props.size === 1
+                ? `${themeGet('space.9')(props)} ${themeGet('space.6')(props)}`
+                : `${themeGet('space.7')(props)} ${themeGet('space.5')(props)}`,
+          },
+        }),
+      _css`
       &::before {
-        animation: ${({ theme, size }: ButtonProps) =>
-          _css`${waitingAnimation({
-            theme,
-            size,
-          })} 500ms linear infinite`}
+        animation: ${waitingAnimation(props)} 500ms linear infinite
       }
     `,
-  { lineHeight: '1' },
-  margin
-);
+      { lineHeight: '1' },
+    ]}
+  />
+));
 
 Button.defaultProps = {
   variant: 'gray',
--- packages/radix/src/components/Divider.tsx
+++ packages/radix/src/components/Divider.tsx
@@ -1,41 +1,34 @@
-import styled from 'styled-components';
+import React from 'react';
 import css from '@styled-system/css';
+import { variant, Prop } from '@modulz/radix-system';
 import {
-  maxWidth,
-  MaxWidthProps,
-  margin,
-  MarginProps,
-  variant,
-  compose,
-  Prop,
-} from '@modulz/radix-system';
+  Divider as DividerPrimitive,
+  DividerProps as DividerPrimitiveProps,
+} from '@modulz/primitives';
 
 type Sizes = 0 | 1 | 2;
-type DividerProps = MaxWidthProps & MarginProps & { size?: Prop<Sizes> };
+type DividerProps = DividerPrimitiveProps & { size?: Prop<Sizes> };
 
-const styleProps = compose(
-  maxWidth,
-  margin
-);
-
-export const Divider = styled.div<DividerProps>(
-  css({
-    backgroundColor: 'grays.2',
-    height: '1px',
-    width: '100%',
-  }),
-  variant({
-    size: {
-      0: {
-        maxWidth: 3,
-      },
-      1: {
-        maxWidth: 6,
-      },
-      2: {
-        maxWidth: 9,
-      },
-    },
-  }),
-  styleProps
+export const Divider = (props: DividerProps) => (
+  <DividerPrimitive
+    {...props}
+    css={[
+      css({
+        backgroundColor: 'grays.2',
+      }),
+      variant({
+        size: {
+          0: {
+            maxWidth: 3,
+          },
+          1: {
+            maxWidth: 6,
+          },
+          2: {
+            maxWidth: 9,
+          },
+        },
+      }),
+    ]}
+  />
 );
