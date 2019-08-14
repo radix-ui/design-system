6c8f3344ebbab4d8644a4d2fd47bbd222d953288
Pedro Duarte <contact@peduarte.com>
2019-08-14 20:09:37 +0100

    feat(radix-system): update border radius

--- packages/radix-system/src/system/borderRadius.ts
+++ packages/radix-system/src/system/borderRadius.ts
@@ -4,11 +4,31 @@ import { Prop, Length } from '../utils/types';
 
 export interface BorderRadiusProps {
   borderRadius?: Prop<CSS.BorderRadiusProperty<Length>>;
+  borderTopLeftRadius?: Prop<CSS.BorderTopLeftRadiusProperty<Length>>;
+  borderTopRightRadius?: Prop<CSS.BorderTopRightRadiusProperty<Length>>;
+  borderBottomLeftRadius?: Prop<CSS.BorderBottomLeftRadiusProperty<Length>>;
+  borderBottomRightRadius?: Prop<CSS.BorderBottomRightRadiusProperty<Length>>;
 }
 
 const config: Config = {
   borderRadius: {
     property: 'borderRadius',
+    scale: 'radii',
+  },
+  borderTopLeftRadius: {
+    property: 'borderTopLeftRadius',
+    scale: 'radii',
+  },
+  borderTopRightRadius: {
+    property: 'borderTopRightRadius',
+    scale: 'radii',
+  },
+  borderBottomLeftRadius: {
+    property: 'borderBottomLeftRadius',
+    scale: 'radii',
+  },
+  borderBottomRightRadius: {
+    property: 'borderBottomRightRadius',
     scale: 'radii',
   },
 };
