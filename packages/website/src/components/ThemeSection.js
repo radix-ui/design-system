f0bb2d4d2063f11a2dd19a9fb5bff36fb0218182
Pedro Duarte <contact@peduarte.com>
2019-08-15 17:59:15 +0100

    docs: update Theme page

--- packages/website/src/components/ThemeSection.js
+++ packages/website/src/components/ThemeSection.js
@@ -1,15 +1,41 @@
 import React from 'react';
-import { theme, Heading, AspectRatio, Grid, Card, Box, Text } from '@modulz/radix';
+import {
+  theme,
+  Heading as RadixHeading,
+  AspectRatio,
+  Grid,
+  Card,
+  Box,
+  Text,
+  Divider,
+} from '@modulz/radix';
 
-// console.log(theme);
+console.log(theme);
 
 export function ThemeSection() {
   return (
     <>
-      <Heading as="h2" fontWeight={500} size={2} mt={8} mb={4}>
-        Colors
-      </Heading>
+      <Heading>Fonts</Heading>
+      {Object.entries(theme.fonts).map(([key, value]) => (
+        <Box mb={3}>
+          <Text as="p" fontFamily={key} size={5}>
+            Voix ambiguë d’un cœur qui, au zéphyr, préfère les jattes de kiwis.
+          </Text>
+        </Box>
+      ))}
 
+      <Divider my={8} />
+
+      <Heading>Font Sizes</Heading>
+      {Object.entries(theme.fontSizes).map(([key, value]) => (
+        <Text size={5} ml={2}>
+          <span style={{ fontSize: value }}>Aa</span>
+        </Text>
+      ))}
+
+      <Divider my={8} />
+
+      <Heading>Colors</Heading>
       <Grid gridTemplateColumns="repeat(4, 1fr)" gridGap={4}>
         {Object.entries(theme.colors).map(([key, value]) => {
           if (Array.isArray(value)) {
@@ -43,4 +69,8 @@ const ColorCard = ({ color, name }) => (
       </Text>
     </Box>
   </Box>
+);
+
+const Heading = props => (
+  <RadixHeading as="h2" fontWeight={500} size={2} mt={8} mb={4} {...props} />
 );
