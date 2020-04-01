0cfcfc33f5a273ac4d2f90591810e477ad356d36
Vlad Moroz <vlad@modulz.app>
2020-04-01 12:51:31 +0300

    Add a larger Avatar size (#241)
    * Added a new Avatar size
    
    * Line breaks in the example

--- packages/radix/src/components/Avatar.story.tsx
+++ packages/radix/src/components/Avatar.story.tsx
@@ -5,12 +5,22 @@ import { Flex } from './Flex';
 import { Avatar } from './Avatar';
 
 storiesOf('Components|Avatar', module).add('default', () => (
-  <Flex>
-    <Box m={2}>
-      <Avatar>AV</Avatar>
-    </Box>
-    <Box m={2}>
-      <Avatar alt="Modulz" src="https://avatars0.githubusercontent.com/u/28682402" />
-    </Box>
-  </Flex>
+  <>
+    <Flex>
+      <Box m={2}>
+        <Avatar>AV</Avatar>
+      </Box>
+      <Box m={2}>
+        <Avatar alt="Modulz" src="https://avatars0.githubusercontent.com/u/28682402" />
+      </Box>
+    </Flex>
+    <Flex>
+      <Box m={2}>
+        <Avatar size={1}>AV</Avatar>
+      </Box>
+      <Box m={2}>
+        <Avatar size={1} alt="Modulz" src="https://avatars0.githubusercontent.com/u/28682402" />
+      </Box>
+    </Flex>
+  </>
 ));
