1f49028e8110aac36ed5f9821668d5ad175f9bae
Vlad Moroz <vlad@modulz.app>
2020-04-13 21:46:14 +0300

    Add Subheading component (#250)
    * Added Subheading
    
    * Body copy updates
    
    * Updated a comment
    
    * Removed a duplicate style

---
+++ packages/radix/src/components/Subheading.tsx
@@ -0,0 +1,72 @@
+import React from 'react';
+import { Text as TextPrimitive, TextProps as TextPrimitiveProps } from '@modulz/primitives';
+import { theme } from '../theme';
+
+type Size = 0 | 1;
+
+export type SubheadingProps = TextPrimitiveProps & {
+  size?: Size;
+  truncate?: boolean;
+};
+
+export const Subheading = React.forwardRef<HTMLHeadingElement, SubheadingProps>(
+  (props, forwardedRef) => (
+    <TextPrimitive
+      {...props}
+      ref={forwardedRef}
+      styleConfig={{
+        base: {
+          text: {
+            normal: {
+              fontFamily: theme.fonts.normal,
+              fontVariant: 'all-small-caps',
+              fontWeight: 500,
+              color: theme.colors.gray800,
+              letterSpacing: '0.035em',
+
+              // Chrome doesn't use kerning on the faux small caps otherwise
+              textTransform: 'uppercase',
+            },
+          },
+        },
+        variants: {
+          size: {
+            0: {
+              text: {
+                normal: {
+                  fontSize: theme.fontSizes[3],
+                  lineHeight: theme.lineHeights[1],
+                },
+              },
+            },
+            1: {
+              text: {
+                normal: {
+                  fontSize: theme.fontSizes[5],
+                  lineHeight: theme.lineHeights[1],
+                },
+              },
+            },
+          },
+          truncate: {
+            true: {
+              text: {
+                normal: {
+                  whiteSpace: 'nowrap',
+                  textOverflow: 'ellipsis',
+                  overflow: 'hidden',
+                },
+              },
+            },
+          },
+        },
+      }}
+    />
+  )
+);
+
+Subheading.defaultProps = {
+  as: 'h2',
+  size: 0,
+  truncate: false,
+};
