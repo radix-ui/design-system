7edf0398faede0601eaa39892856b7c35fdef336
Vlad Moroz <vlad@modulz.app>
2020-04-24 12:12:47 +0300

    Fix the input baseline position (#254)
    * Fixed the wobbly input baseline
    
    * Fixed the input baseline wobble

--- packages/radix/src/components/Input.tsx
+++ packages/radix/src/components/Input.tsx
@@ -76,6 +76,27 @@ export const Input = React.forwardRef<HTMLInputElement, InputProps>((props, forw
                 lineHeight: theme.lineHeights[0], // Yields nice text selection height in Safari
                 paddingLeft: theme.space[2],
                 paddingRight: theme.space[2],
+
+                // A workaround for a tiny visual bug in Chrome
+                //
+                // When input is positioned ambiguously — imagine it's vertically centered on the screen
+                // so its position seems to be calculated with subpixel-level precision — there is a schism
+                // between how the actual input “box” is placed and how its text baseline is placed. In this
+                // case resizing the viewport height may introduce slight baseline misalignments and make
+                // text look not centered vertically within the input.
+                //
+                // This happens because actual input text is rendered by a shadow DOM element, which doesn't use
+                // the line height of the input. Instead, it uses the default "line-height: normal", which means 1.2.
+                //
+                // So:
+                // * The input height is 35px, the font size is 13px
+                // * The shadow DOM element height is Math.ceil(13px * 1.2) = 16px
+                // * 35px - 16px = 19px for the browser to allocate above and below the shadow DOM element
+                //
+                // Hence adding the 1px padding to resolve the mismatch between the odd input height (35px) and
+                // the even shadow DOM value element height (16px). This way baseline position stays consistent
+                // no matter how the input is placed.
+                paddingBottom: '1px',
               },
             },
           },
