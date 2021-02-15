6625c2143664503b73b32f3f95311a12b4898e56
Pedro Duarte <contact@peduarte.com>
2021-02-15 16:50:03 +0100

    Fix control group

--- components/ControlGroup.tsx
+++ components/ControlGroup.tsx
@@ -9,7 +9,8 @@ export const ControlGroup = styled('div', {
 
   '& button': {
     borderRadius: 0,
-    boxShadow: 'inset 0 1px $colors$gray600, inset -1px 0 $colors$gray600, inset 0 -1px $colors$gray600',
+    boxShadow:
+      'inset 0 1px $colors$gray600, inset -1px 0 $colors$gray600, inset 0 -1px $colors$gray600',
     '&:hover': {
       boxShadow:
         '-1px 0 $colors$gray700, inset 0 1px $colors$gray700, inset -1px 0 $colors$gray700, inset 0 -1px $colors$gray700',
@@ -24,16 +25,24 @@ export const ControlGroup = styled('div', {
       '&:hover': {
         boxShadow: 'inset 0 0 0 1px $colors$gray700',
       },
+      '&:focus': {
+        boxShadow: 'inset 0 0 0 1px $colors$gray700, 0 0 0 1px $colors$gray700',
+      },
     },
     '&:last-child': {
       borderTopRightRadius: '$2',
       borderBottomRightRadius: '$2',
-      boxShadow: 'inset 0 1px $colors$gray600, inset -1px 0 $colors$gray600, inset 0 -1px $colors$gray600',
+      boxShadow:
+        'inset 0 1px $colors$gray600, inset -1px 0 $colors$gray600, inset 0 -1px $colors$gray600',
+      '&:focus': {
+        boxShadow: 'inset 0 0 0 1px $colors$gray700, 0 0 0 1px $colors$gray700',
+      },
     },
   },
   '& input': {
     borderRadius: 0,
-    boxShadow: 'inset 0 1px $colors$gray600, inset -1px 0 $colors$gray600, inset 0 -1px $colors$gray600',
+    boxShadow:
+      'inset 0 1px $colors$gray600, inset -1px 0 $colors$gray600, inset 0 -1px $colors$gray600',
     '&:focus': {
       zIndex: 1,
     },
@@ -45,7 +54,8 @@ export const ControlGroup = styled('div', {
     '&:last-child': {
       borderTopRightRadius: '$2',
       borderBottomRightRadius: '$2',
-      boxShadow: 'inset 0 1px $colors$gray600, inset -1px 0 $colors$gray600, inset 0 -1px $colors$gray600',
+      boxShadow:
+        'inset 0 1px $colors$gray600, inset -1px 0 $colors$gray600, inset 0 -1px $colors$gray600',
     },
   },
 });
