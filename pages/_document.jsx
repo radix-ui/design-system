9657c38202eddf73b7e1544cf0755f667e190e1f
Pedro Duarte <contact@peduarte.com>
2021-02-24 14:19:42 +0100

    Update _document.jsx

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -3,27 +3,12 @@ import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
 import { getCssString } from '../stitches.config';
 
 export default class Document extends NextDocument {
-  static async getInitialProps(ctx) {
-    try {
-      const initialProps = await NextDocument.getInitialProps(ctx);
-
-      return {
-        ...initialProps,
-        styles: (
-          <>
-            {initialProps.styles}
-            <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssString() }} />
-          </>
-        ),
-      };
-    } finally {
-    }
-  }
-
   render() {
     return (
       <Html lang="en">
-        <Head />
+        <Head>
+          <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssString() }} />
+        </Head>
         <body>
           <Main />
           <NextScript />
