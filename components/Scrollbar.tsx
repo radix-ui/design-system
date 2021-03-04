8ade6b40f2bea66b7a8dd3206e130b822c6c9e6a
Benoît Grélard <benoit.grelard@gmail.com>
2021-03-04 17:38:12 +0000

    Upgrade primitives + SSR support + default Accordion to single

--- components/Scrollbar.tsx
+++ components/Scrollbar.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Flex } from '../components/Flex';
 import { Box } from '../components/Box';
-import { useLayoutEffect } from '@radix-ui/react-utils';
+import { useLayoutEffect } from '@radix-ui/react-use-layout-effect';
 
 type Point = {
   x: number;
