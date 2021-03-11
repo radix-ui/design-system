864076362ec2cfb92f7fe47cc58a769613b2dd66
Colm Tuite <colmtuite@gmail.com>
2021-03-11 20:12:03 +0000

    Convert Badge to token

--- components/Badge.tsx
+++ components/Badge.tsx
@@ -46,7 +46,7 @@ const StyledBadge = styled(DEFAULT_TAG, {
   variants: {
     size: {
       '1': {
-        height: '20px',
+        height: '$4',
         px: '$1',
         fontSize: '$1',
       },
