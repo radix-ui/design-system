705500a628563a998b38ba3d2792c132841a559b
Colm Tuite <colmtuite@gmail.com>
2021-03-11 20:08:54 +0000

    Clean some stuff up

--- components/ControlGroup.tsx
+++ components/ControlGroup.tsx
@@ -69,30 +69,4 @@ export const ControlGroup = styled('div', {
       },
     },
   },
-  [`${Select}`]: {
-    borderRadius: 0,
-    boxShadow:
-      'inset 0 1px $colors$slate600, inset -1px 0 $colors$slate600, inset 0 -1px $colors$slate600',
-    '&:focus': {
-      zIndex: 1,
-      boxShadow: 'inset 0px 0px 0px 1px $colors$blue700, 0px 0px 0px 1px $colors$blue700',
-    },
-    '&:first-child': {
-      borderTopLeftRadius: '$2',
-      borderBottomLeftRadius: '$2',
-      boxShadow: 'inset 0 0 0 1px $colors$slate600',
-      '&:focus': {
-        boxShadow: 'inset 0px 0px 0px 1px $colors$blue700, 0px 0px 0px 1px $colors$blue700',
-      },
-    },
-    '&:last-child': {
-      borderTopRightRadius: '$2',
-      borderBottomRightRadius: '$2',
-      boxShadow:
-        'inset 0 1px $colors$slate600, inset -1px 0 $colors$slate600, inset 0 -1px $colors$slate600',
-      '&:focus': {
-        boxShadow: 'inset 0px 0px 0px 1px $colors$blue700, 0px 0px 0px 1px $colors$blue700',
-      },
-    },
-  },
 });
