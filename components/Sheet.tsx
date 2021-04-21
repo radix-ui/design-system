28f91c30959951c95f68c68abf007ed40c50fcc6
Colm Tuite <colmtuite@gmail.com>
2021-04-21 11:59:08 +0100

    Fix Sheet shadow

--- components/Sheet.tsx
+++ components/Sheet.tsx
@@ -60,6 +60,7 @@ const slideOut = keyframes({
 
 const StyledContent = styled(DialogPrimitive.Content, {
   backgroundColor: '$panel',
+  boxShadow: '$colors$shadowLight 0 0 38px -10px, $colors$shadowDark 0 0 35px -15px',
   position: 'fixed',
   top: 0,
   bottom: 0,
