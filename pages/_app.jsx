f36da580070ce10e5255ddf98585c71400235ab1
Colm Tuite <colmtuite@gmail.com>
2021-05-12 13:53:51 +0100

    Fix Toggle theme button

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -25,7 +25,7 @@ function App({ Component, pageProps }) {
         <Component {...pageProps} />
 
         <Button
-          variant="ghost"
+          ghost
           style={{ position: 'fixed', zIndex: 999, right: 15, top: 15 }}
           onClick={() => setTheme(theme === 'theme-default' ? darkTheme : 'theme-default')}
         >
