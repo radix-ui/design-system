6d9f6ea881c3abd2cc103a9cbde7c0d96d562877
Pedro Duarte <contact@peduarte.com>
2021-06-09 11:32:49 +0200

    Update Heading.tsx

--- components/Heading.tsx
+++ components/Heading.tsx
@@ -31,26 +31,10 @@ export const Heading = React.forwardRef((props, forwardedRef) => {
 
   // This is the mapping of Heading Variants to Text css
   const textCss: Record<HeadingSizeVariants, CSS> = {
-    1: {
-      fontWeight: 500,
-      lineHeight: '20px',
-      '@bp2': { lineHeight: '23px' },
-    },
-    2: {
-      fontWeight: 500,
-      lineHeight: '25px',
-      '@bp2': { lineHeight: '30px' },
-    },
-    3: {
-      fontWeight: 500,
-      lineHeight: '33px',
-      '@bp2': { lineHeight: '41px' },
-    },
-    4: {
-      fontWeight: 500,
-      lineHeight: '35px',
-      '@bp2': { lineHeight: '55px' },
-    },
+    1: { fontWeight: 500, lineHeight: '20px', '@bp2': { lineHeight: '23px' } },
+    2: { fontWeight: 500, lineHeight: '25px', '@bp2': { lineHeight: '30px' } },
+    3: { fontWeight: 500, lineHeight: '33px', '@bp2': { lineHeight: '41px' } },
+    4: { fontWeight: 500, lineHeight: '35px', '@bp2': { lineHeight: '55px' } },
   };
 
   return (
