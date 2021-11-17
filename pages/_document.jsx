56322aae6984e580998e78b556215fb4b843c2e5
Abdul <hallak.aa@gmail.com>
2021-11-17 17:35:39 +0400

    Make sure to use reset after getting the css

--- pages/_document.jsx
+++ pages/_document.jsx
@@ -1,13 +1,24 @@
 import React from 'react';
 import NextDocument, { Html, Head, Main, NextScript } from 'next/document';
-import { getCssText } from '../stitches.config';
+import { getCssText, reset } from '../stitches.config';
+
+/**
+ * Get the css and reset the internal css representation.
+ * This is very *IMPORTANT* to do as the server might handle multiple requests
+ * and we don't want to have the css accumulated from previous requests
+ */
+const getCssAndReset = () => {
+  const css = getCssText();
+  reset();
+  return css;
+};
 
 export default class Document extends NextDocument {
   render() {
     return (
       <Html lang="en">
         <Head>
-          <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssText() }} />
+          <style id="stitches" dangerouslySetInnerHTML={{ __html: getCssAndReset() }} />
         </Head>
         <body>
           <Main />
