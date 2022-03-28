4eaab9632a82a7bad157fdcdf1b3bf2f9255a4fa
Guilherme Costa <guilherme.rodrigues.costa@outlook.com>
2022-03-28 13:30:17 -0700

    fix typo on heading text (#371)
    This is a simple PR to replace `orientaiton` with `orientation`

--- pages/index.tsx
+++ pages/index.tsx
@@ -2184,7 +2184,7 @@ function Home() {
             <Flex css={{ gap: '$4', width: '150px', mb: '$7' }}>
               <Slider defaultValue={[25, 75]} />
             </Flex>
-            <Heading css={{ mb: '$6' }}>Vertical orientaiton</Heading>
+            <Heading css={{ mb: '$6' }}>Vertical orientation</Heading>
             <Box css={{ mt: '$6' }}>
               <Slider defaultValue={[50]} orientation="vertical" css={{ height: 75 }} />
             </Box>
